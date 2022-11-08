2c5dc3553582f4ec989224941088e3c64131ab89
CRIMS0NKING <116672891+CRIMS0NKING@users.noreply.github.com>
2022-11-08 11:43:37 -0800

    fixing
    Fixed Errors,  designed world

--- RubysAdventureGuanzing/Assets/Scripts/RubyController.cs
+++ RubysAdventureGuanzing/Assets/Scripts/RubyController.cs
@@ -1,3 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 public class RubyController : MonoBehaviour
 {
     // Start is called before the first frame update
